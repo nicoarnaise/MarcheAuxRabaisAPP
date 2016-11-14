827c1cdae24e4434eecc3f2d1ba891b1e3c52e91
fredericRignault <rignaultfrederic@gmail.com>
2016-11-14 11:28:09 -0500

    Affiche une carte + GPS + Centre la carte sur l'utilisateur + Marqueur

--- MarcheAuxRabais/app/src/main/java/com/geasser/marcheauxrabais/MapsActivity.java
+++ MarcheAuxRabais/app/src/main/java/com/geasser/marcheauxrabais/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.geasser.marcheauxrabais;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +20,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    // Localisation en plein dans le Tchad
+    private double mLatitude = 20;
+    private double mLongitude = 20;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +39,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        // Create an instance of GoogleAPIClient, nécessaire pour les services de géolocalisation.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,10 +62,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        // LatLng(Latitude,Longitude)
+        LatLng chicoutimi = new LatLng(48.4222, -71.0619);
+        // Ajoute un marqueur rouge sur la carte à la latitude et la longitude de Chicoutimi
+        mMap.addMarker(new MarkerOptions().position(chicoutimi).title("Marker in Chicoutimi"));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Obligatoire d'avoir l'autorisation de l'utilisateur pour la localisation.
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+    }
+
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+    
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        // Si l'utilisateur accepte de partager sa localisation alors ...
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            //Récupération des coordonnées GPS de la localisation GPS
+            if (mLastLocation != null) {
+                mLatitude=mLastLocation.getLatitude();
+                mLongitude=mLastLocation.getLongitude();
+                LatLng test = new LatLng(mLatitude,mLongitude);
+                // Centre la carte sur l'utilisateur avec un zoom de 16.
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(test,16));
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
